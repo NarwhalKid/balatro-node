59ce8878188e3b0385300337bf8884de29d5fbdf
Narwhal Kid <55326739+NarwhalKid@users.noreply.github.com>
2025-05-02 13:27:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3182,7 +3182,7 @@ function newGame(deck = "Red Deck", stake = "White Stake") {
             break;
         case "magicdeck":
             addVoucher(game, "Crystal Ball");
-            const fool = objectClone(cards["Tarot Card"].find(card => card.name == "The Fool"));
+            const fool = objectClone(cards["Tarot Card"].find(card => card.name == "Lovers"));
             fool.cost = 3;
             game.consumables.push(objectClone(fool));
             game.consumables.push(fool);
