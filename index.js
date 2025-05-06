b6aa9cb8bf0471b7649c629ba7fb82e7bf27dde5
Narwhal Kid <madgamer2525@gmail.com>
2025-05-06 14:51:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4192,6 +4192,7 @@ function restoreGameFunctions(game) {
   sections.forEach(section => {
     if (!Array.isArray(section)) return;
     section.forEach(consumable => {
+      if (!consumable) return;
       if (consumable.handType) consumable.getDesc = (gameState) => {return `(lvl.${gameState.handLevels[consumable.handType]}) Level up ${consumable.handType}`};
       const ogConsumable = deepFind(cards, findConsumable => findConsumable?.name == consumable.name) || deepFind(jokers, findConsumable => findConsumable?.name == consumable.name) || deepFind(tags, findConsumable => findConsumable?.name == consumable.name);
       if (!ogConsumable) return;
