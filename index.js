95f640b1d51c752c006cc6214df0c8f021d4f91c
Narwhal Kid <madgamer2525@gmail.com>
2025-05-08 15:06:19 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4547,8 +4547,8 @@ function handleBlind(gameState, func, props = []) {
         card.selected = false;
         return;
       }
-      if (gameState.blind?.isDebuffed) card.debuffed = gameState.blind.isDebuffed;
-      if (gameState.blind?.isFaceCard) card.flipped = gameState.blind.isFaceCard;
+      if (gameState.blind?.isDebuffed) card.debuffed = gameState.blind.isDebuffed();
+      if (gameState.blind?.isFaceCard) card.flipped = gameState.blind.isFaceCard();
     })
   } else if (gameState.blind?.[func]) {
     if (gameState.blind?.disabled) return;
